aeb9668b880f0808ff8554424426a7e0ba33d2a4
Peter_Lin <a31438199@gmail.com>
2025-06-05 03:50:05 +0800

    "feat": Adding annotations.

--- action.py
+++ action.py
@@ -26,19 +26,18 @@ def collision(ball,object):
     dy = (ball.rect.centery - object.rect.centery)
     #如果兩個矩形中心點距離 dx 小於 widths，代表水平方向有重疊。
     #widths 就像是從兩物件中心延伸的邊界距離。
-
     widths = (ball.rect.width + object.rect.width) / 2
     heights = (ball.rect.height + object.rect.height) / 2
-
+    #兩個的一半長度-兩個中心點的長度
     overlap_x = widths - abs(dx)
     overlap_y = heights - abs(dy)
-
+    
     if overlap_x < overlap_y:
         direction = "left" if dx < 0 else "right"
     else:
         direction = "top" if dy < 0 else "bottom"
 
-            
+    #根據撞擊的面決定球的速度方向      
     if direction == "top":
         if ball.speed_x >= 0 and ball.speed_y >= 0:
             ball.speed_x = abs(ball.speed_x)
@@ -84,7 +83,7 @@ def collision(ball,object):
             print("h")  
             
     return ball.speed_x,ball.speed_y
-
+#刪除磚塊與特殊磚塊動作
 def tile_action(ball,tiles,projectile_cls,ball_cls,ball_image,heal_image,longbar_image,audio):
     config.to_remove = []
     check_bounce_tileBreak(ball,tiles,config.to_remove)
@@ -101,17 +100,19 @@ def tile_action(ball,tiles,projectile_cls,ball_cls,ball_image,heal_image,longbar
             config.score_text = score()
             # tile.deleted = True
             
-
+#以上一偵的rect來判斷是否撞擊
 def check_bounce_tileBreak(ball,tiles,to_remove):
     ball_range = ball.rect.inflate(50,50)
     for tile in tiles:
+        #如果膨脹後的rect沒有碰到 continue
         if not ball_range.colliderect(tile.rect):
             continue
+        #碰到後 判斷 上一偵的rect沒碰到磚塊 現在的rect碰到磚塊 就判斷從哪裡的(collision) 得到新的球速度, 將磚塊加入待刪除
         if not circle_rect_collide(ball.previous_rect.center,ball.offset_x/2,tile.rect) and circle_rect_collide(ball.rect.center,ball.offset_x/2,tile.rect):
             ball.speed_x,ball.speed_y = collision(ball,tile)
             to_remove.append(tile)
             
-            
+#一開始設想會有多種pattern, 暫時擱置, 磚塊打完就下一回合 進入tile_pattern 生成磚塊組            
 def tile_generation(tiles,tile,image,screen,audio):
     pattern = random.randint(0,0)
     draw_round(screen)
@@ -120,16 +121,14 @@ def tile_generation(tiles,tile,image,screen,audio):
     if len(tiles) == 0:
         if config.round != 1:
             config.balls.clear()
-            config.tick += 30
-            print(config.tick)
+            config.tick += 50
         config.round_text = round()
         tile_pattern(pattern,tiles,tile,image,audio) 
         audio[3].play()
         config.round += 1
-        print(config.tick)
         
     return tiles
-
+#判斷普通與特殊磚塊的生成機率
 def tile_type():
     num = random.randint(0,20)
     type = "normal"
@@ -156,10 +155,10 @@ def draw_round(screen):
 def draw_total_score(screen):
     config.total_points_text = config.menu_font.render(f"You got {config.points}",False,(255,255,255))
     screen.blit(config.total_points_text,config.total_points_rect)
-    
+#新增球的工廠函數   
 def add_balls(image,cls,x_pos,y_pos,speed_x,speed_y,if_original):
     return cls(image,x_pos,y_pos,speed_x,speed_y,if_original)
-
+#如果沒有金球就補一個
 def refill_ball(image,ball_cls,bar1):
     original_count = 0
     for ball in config.balls:
@@ -168,10 +167,9 @@ def refill_ball(image,ball_cls,bar1):
             
     if not original_count and config.has_initialized:
         config.balls.append(add_balls(image[0],ball_cls,bar1.rect.center[0]-image[0].get_width()/2,
-        config.screen_height - 100 -image[0].get_height(),0,0,True))
-    #     config.ball_refilled = True
-    # config.ball_refilled = False     
+        config.screen_height - 100 -image[0].get_height(),0,0,True))  
         
+#得到新球後可以決定從左還是右邊射出     
 def shoot_ball(userinput):
 
     for ball in config.balls:
@@ -182,13 +180,13 @@ def shoot_ball(userinput):
             if userinput[pygame.K_RIGHT]:
                 ball.speed_x = config.ballspeed_x
                 ball.speed_y = config.ballspeed_y
-
+#球超過下邊界就會被刪除
 def delete_balls(ball,health,audio):
     if ball.rect.y >= config.screen_height:
         if ball in config.balls:
             config.balls.remove(ball)
             take_damage(health,ball,audio)
-
+#金球被刪除會扣血
 def take_damage(health,ball,audio):
     if ball.original_ball:
         audio.play()
@@ -202,6 +200,7 @@ def draw_tiles(screen):
         tile.update()
         tile.draw(screen)
 
+#遊戲結束時初始化與清除所有變數
 def game_stat_end(audio):
     audio[4].play()
     config.main_run = False
@@ -213,7 +212,7 @@ def game_stat_end(audio):
     config.ball_refilled = False
     config.has_initialized = False
     
-
+#新增球的特殊磚塊
 def tile_add_new_ball(tile,ball_cls,ball_image,ball):
     if tile.type == "new_ball":
         new_momentum_x = random.randint(-1,2)
@@ -225,11 +224,11 @@ def tile_add_new_ball(tile,ball_cls,ball_image,ball):
             new_ballspeed_y = random.randint(3,5)
         image_num = random.randint(1,3)
         config.balls.append(add_balls(ball_image[image_num],ball_cls,tile.rect.center[0],tile.rect.center[1],new_ballspeed_x,new_ballspeed_y,False))
-        
+#回復血量的特殊磚塊       
 def tile_heal(tile,projectile_cls,image):
     if tile.type == "heal":
         config.projectiles.append(projectile_cls(image,tile.rect.center[0],tile.rect.center[1],config.projectile_speed_x,config.projectile_speed_y,"heal"))
-        
+#讓桿子變大的特殊磚塊      
 def tile_longbar(tile,projectile_cls,image):
     if  tile.type == "longbar":
         config.projectiles.append(projectile_cls(image,tile.rect.center[0],tile.rect.center[1],config.projectile_speed_x,config.projectile_speed_y,"longbar"))
@@ -254,7 +253,7 @@ def projectile_update(screen,bar_rect,health_obj):
         projectile.update(bar_rect,health_obj)
         projectile.draw(screen)
         
-        
+#讓桿子變長10秒，如果還沒結束就加到10秒       
 def long_bar(bar,bar_image,longbar_image):
     bar.offset_x = bar_image.get_width()
     bar.offset_y = bar_image.get_height()
@@ -273,7 +272,7 @@ def long_bar(bar,bar_image,longbar_image):
         bar.rect.center =(old_bar_centerx,old_bar_centery)
         bar.offset_y = longbar_image.get_height()
         bar.offset_x = longbar_image.get_width()
-        if config.longbar_multiple_hit > 2:
+        if config.longbar_multiple_hit >= 2:
             print(config.longbar_multiple_hit)
             config.end_time += (10-(config.end_time - time.time()))
             #回到只打中一次的hit
@@ -285,7 +284,7 @@ def long_bar(bar,bar_image,longbar_image):
         bar.image = bar_image
         bar.rect = bar_image.get_rect()
         bar.rect.center =(old_bar_centerx,old_bar_centery)
-         
+#實際生成磚塊組進陣列裡 
 def tile_pattern(pattern,tiles,tile_cls,image,audio):
     if pattern == 0:
         x_start_pos = 65
@@ -309,7 +308,7 @@ def tile_pattern(pattern,tiles,tile_cls,image,audio):
                 x_pos += (image[0].get_width() + gap)
             x_pos = x_start_pos
             y_pos += image[0].get_height() + 10
-            
+#爆炸的特殊磚塊 
 def tile_explode(tile):
     if tile.type == "explode":
         explosion_rect = tile.rect.inflate(tile.image.get_width(),tile.image.get_height())
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import config
 import action
 
 
-
+# 初始化 pygame 套件，然後建立遊戲視窗、標題、背景等
 pygame.init()
 pygame.mixer.init(frequency=44100, size=-16, channels=2)
 screen = pygame.display.set_mode((config.screen_width,config.screen_height))
@@ -60,6 +60,7 @@ class GameObject:
     
     def __init__(self,image):
         self.image = image
+        # assert 讓傳進來的圖片，資料型態只能是pygame.Surface
         assert isinstance(self.image, pygame.Surface)
         self.rect = self.image.get_rect()
         
@@ -73,14 +74,20 @@ class GameObject:
 class Ball(GameObject):
     def __init__(self,image,x_pos,y_pos,speed_x,speed_y,if_original):   
         super().__init__(image)
+        # 自己物件本身現在的速度
         self.speed_x = speed_x
         self.speed_y = speed_y
+        # 這裡的變數是用來判斷物件是否有超出螢幕 (針對畫圖的部分)
         self.offset_x = image.get_width()
         self.offset_y = image.get_height()
+        # 初始化球體的x跟y的座標
         self.rect.x = x_pos
         self.rect.y = y_pos
+        # 上一幀的矩形位置，copy就是把矩形複製一份
         self.previous_rect = self.rect.copy()
+        # 新增的球不會扣血
         self.original_ball = if_original
+    # 負責球體的更新，判斷不要超過螢幕邊框，更新球體位置
     def update(self):
         if self.rect.x <= 0:
             AUDIO[1].play()
@@ -104,21 +111,25 @@ class Ball(GameObject):
         
 
         
-
+#磚頭的類別
 class tile(GameObject):
     def __init__(self,image,x_pos,y_pos,type,audio):
         super().__init__(image)
         self.image = image.copy()
         self.rect.x = x_pos
         self.rect.y = y_pos
+         # 磚頭有不同種類 (一般、補血、爆炸、加新球、加長桿子)
         self.type = type
+        # 設定 alpha 也是 pygame 的東西
         self.alpha = 255
+        # 判斷要不要變透明
         self.fading = False
         self.deleted = False
         self.audio = audio
         self.audio.set_volume(0.2)
-    
+    # 判斷動畫更新，如果磚頭被打到的話會開始慢慢消失
     def update(self):
+        # 若正在消失且在被移除的陣列裡面
         if self.fading == True and self in config.removed:
             self.image = TILE[10]
             self.alpha = max(0, self.alpha - 15)
@@ -133,15 +144,17 @@ class tile(GameObject):
             
 
         
-
+# 平台的類別
 class bar(GameObject):
     def __init__(self,image):
         super().__init__(image)
+        # 這裡的變數是用來判斷物件是否有超出螢幕 (針對畫圖的部分)
         self.offset_x = self.image.get_width()
         self.offset_y = self.image.get_height()
         self.rect.x = config.screen_width / 2 - (self.offset_x/2)
         self.rect.y = 650 - (self.offset_y/2)
         self.speed = 10
+    # 處理按下方向鍵後，桿子的移動，讓桿子的圖片呈現不要超出邊框
     def update(self,userinput):
         if userinput[pygame.K_LEFT] and not userinput[pygame.K_RIGHT]:
             self.rect.x -= self.speed
@@ -165,7 +178,7 @@ class background:
             self.image = image
     def draw(self,screen):
         screen.fill((53,40,90))
- 
+# 血量類別
 class health:
     def __init__(self):
         self.image = HEALTH[0] 
@@ -177,11 +190,15 @@ class health:
         self.half_image = HEALTH[1] 
         self.empty_image = HEALTH[2]
         self.full_hp = 6
+        # 初始化目前的血量為滿血
         self.current_hp = self.full_hp
+        # 此變數是損失的血量 (起始化為0)
         self.lost_hp = 0
+        # 畫畫用
         self.half_heart_count = 0                
         self.empty_heart_count = 0                    
         self.full_heart_count = int(self.current_hp / 2)
+    # 處理血量的呈現 (此處用拼接呈現)
     def update(self,screen):
         self.rect.x = 10
         self.right_end = self.rect.x
@@ -207,7 +224,7 @@ class health:
         if self.current_hp <= 0 and config.main_run:
             print(self.current_hp)
             action.game_stat_end(AUDIO)
-            
+# 投射物的類別 (如愛心、長桿等道具 (會垂直往下的那些))            
 class Projectile(GameObject):
     def __init__(self,image,pos_x,pos_y,speed_x,speed_y,type):
         super().__init__(image)
@@ -216,8 +233,10 @@ class Projectile(GameObject):
         self.rect.y = pos_y
         self.speed_x = speed_x
         self.speed_y = speed_y
+        # 一樣有不同種類的投射物
         self.type = type
     def update(self,bar_rect,health_obj):
+        # 矩形的碰撞檢測，偵測撞到桿子後會有的行為
         if self.rect.colliderect(bar_rect):
             if self.type == "heal":
                 if health_obj.current_hp >= health_obj.full_hp:
@@ -242,10 +261,13 @@ class Projectile(GameObject):
             
     
 def main():
+    #初始化round, points
     config.points = 0
     config.round = 1
+    # 建立幀率的物件
     clock = pygame.time.Clock()
     bar1 = bar(BAR[0])
+     # config 是存放變數的地方，在裡面的球的陣列加入一顆最一開始的球，最後設True是因為他是最一開始的球
     config.balls.append(action.add_balls(BALL[0],Ball,bar1.rect.center[0]-BALL[0].get_width()/2,config.screen_height/2 - BALL[0].get_height()/2, 0, 0,True))
     background1 = background(BACKGROUND)
     health1 = health()
@@ -253,67 +275,77 @@ def main():
     
     while config.main_run:
         for event in pygame.event.get():
+            # 如果要退出的話 (按下esc退出)
             if event.type == pygame.QUIT:
                 config.main_run = False
                 config.menu_run = False
         
-
+        # 畫圖+抓玩家輸入
         background1.draw(screen)
         userinput = pygame.key.get_pressed()
+        # 最一開始操控左右可以決定一開始射球的方向
         action.shoot_ball(userinput)
         config.tiles = action.tile_generation(config.tiles,tile,TILE,screen,AUDIO)
-        
+        # 更新球的圖片與位置
         for ball in config.balls:
             ball.draw(screen)
             ball.update()
-                
+            # 這邊是圓形的碰撞，把球的半徑跟圓心與桿子傳入函數，判斷有沒有碰撞，若有碰撞則執行以下程式    
             if action.circle_rect_collide(ball.rect.center,ball.offset_x/2,bar1.rect):
                 AUDIO[1].play()
-                print(ball.rect)
+                #根據碰撞的方向決定速度 回傳的會是球的新速度x,y 
                 ball.speed_x,ball.speed_y = action.collision(ball,bar1)
-            
+            #刪除球
             action.delete_balls(ball,health1,AUDIO[5])
+            #特殊磚頭被碰到後的行為
             action.tile_action(ball,config.tiles,Projectile,Ball,BALL,PROJECTILE[0],PROJECTILE[1],AUDIO)
+        # 更新投射物
         action.projectile_update(screen,bar1.rect,health1)
+        # 更新血量
         health1.update(screen)
         action.draw_tiles(screen)   
         bar1.draw(screen)
         bar1.update(userinput)
-        
+        # 如果要加球的話，把to_add(待增加)拼接到原本的球陣列上
         if config.to_add:
             config.balls.extend(config.to_add)
             config.to_add.clear()
-        
+        # 把原本的桿子變長桿子
         action.long_bar(bar1,BAR[0],BAR[1])
 
-        
+        # 球沒了會重生一顆球 (original 金球)
         action.refill_ball(BALL,Ball,bar1)
-        
+        # 畫出分數
         action.draw_score(screen)
-        
+        # 畫出 fps
         action.get_fps(clock,screen)
 
         pygame.display.update()
         
-        clock.tick(config.tick)   
+        # 設定fps  
+        clock.tick(config.tick)  
     
 
 
 def menu(): 
     config.menu_run = True
     while config.menu_run:
+        # 初始化選單要有的東西
         screen.fill((203,189,173))
         screen.blit(config.logo_menu_text,config.logo_menu_rect)
         screen.blit(config.start_menu_text,config.start_text_rect)
         pygame.display.update()
+        # 判斷有沒有要退出
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 config.main_run = False
                 config.menu_run = False
             if event.type == pygame.KEYDOWN:
+                # 按下開始後等0.2秒會進入main()
                 pygame.time.wait(200)
                 main()
-        
+        # 輸掉的 menu 會出現在這
+        # 輸的時候判斷會成功，會進入 gameOver menu
         while config.menu_run == True and config.main_run == False:
             screen.fill((0,0,0))
             screen.blit(config.end_menu_text1,config.end_text_rect1)
@@ -324,6 +356,7 @@ def menu():
                 if event.type == pygame.QUIT:
                     config.main_run = False
                     config.menu_run = False
+                # 按下任何按鈕可以再開始一局
                 if event.type == pygame.KEYDOWN:
                     pygame.time.wait(200)
                     config.main_run = True
